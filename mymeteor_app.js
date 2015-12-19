7d515cbcb8b64302365d2fc46b20f4c062c46589
kenzo2013 <kkenzo2007@yahoo.fr>
2015-12-19 22:26:47 +0100

    Fixed all

--- mymeteor_app.js
+++ mymeteor_app.js
@@ -1,30 +1,36 @@
 Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
+  // counter starts at 0
+  Session.setDefault('counter', 0);
   // List all tasks
   Template.body.helpers({
     tasks: function () {
        return Tasks.find({}, {sort: {createdAt: -1}});
-      }
+     },
+     counter: function () {
+       return Session.get('counter');
+     }
   });
 
   //submit form
   Template.body.events({
-    "submit .new-task": function (event) {
+    "submit form": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
 
       // Get value from form element
       var text = event.target.text.value;
-
+      var com = event.target.comment.value;
       // Insert a task into the collection
       Tasks.insert({
         text: text,
+        comment: com,
         createdAt: new Date() // current time
       });
-
       // Clear form
       event.target.text.value = "";
+      event.target.comment.value = "";
     }
   });
   // update and delete task
@@ -35,8 +41,10 @@ if (Meteor.isClient) {
         $set: {checked: ! this.checked}
       });
     },
-    "click .delete": function () {
+     "click .delete": function () {
       Tasks.remove(this._id);
+      // increment the counter when button is clicked
+       Session.set('counter', Session.get('counter') + 1);
     }
   });
 }
