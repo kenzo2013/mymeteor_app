15e89210ee4d1e1c956b5071b069bc9ecc04e17e
kenzo2013 <kkenzo2007@yahoo.fr>
2015-12-19 16:37:28 +0100

    update and remove ok

--- mymeteor_app.js
+++ mymeteor_app.js
@@ -1,12 +1,14 @@
 Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
+  // List all tasks
   Template.body.helpers({
     tasks: function () {
        return Tasks.find({}, {sort: {createdAt: -1}});
       }
   });
 
+  //submit form
   Template.body.events({
     "submit .new-task": function (event) {
       // Prevent default browser form submit
@@ -25,5 +27,16 @@ if (Meteor.isClient) {
       event.target.text.value = "";
     }
   });
-
+  // update and delete task
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
 }
